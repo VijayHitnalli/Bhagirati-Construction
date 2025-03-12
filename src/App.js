30c8c629ba8ca16068a1e57e9233c2be70b5fe34
vijay hitnalli <vijayhitnalli35@gmail.com>
2025-03-12 14:07:33 +0530

    Added Header Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
 
 
 function App() {
   return (
     <div className="App">
       
-     
-    Bhagirati Construction
+      <Router>
+
+        <Header/>
+
+         
+
+
+   
+
+
+     </Router>
+
 
 
     </div>
