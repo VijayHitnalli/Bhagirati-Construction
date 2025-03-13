44d802b6283ab9d8f340f288738d202576130078
vijay hitnalli <vijayhitnalli35@gmail.com>
2025-03-14 01:27:49 +0530

    Added Home Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './components/Home';
 
 
 function App() {
@@ -11,7 +12,12 @@ function App() {
       <Router>
 
         <Header/>
-
+    
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        
+      </Routes>
+    
          
 
 
